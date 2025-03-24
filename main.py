4fd91cf8cef82fe31953acbe9e2ea4b949d0ec2f
Martins Vagins <143533280+mafioznik101@users.noreply.github.com>
2025-03-24 13:27:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -126,4 +126,5 @@ def renderThisPath():
   return res
 # if __name__ == '__main__':  
 #    app.run(debug = True)
-app.run(debug=True,host="0.0.0.0", port=80)
+#app.run(debug=True,host="0.0.0.0", port=80)
+app.run(debug=False,host="0.0.0.0", port=80)
