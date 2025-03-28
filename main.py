c3994725006f3a024af252ed3fbacf6aaeb55687
Martins Vagins <143533280+mafioznik101@users.noreply.github.com>
2025-03-28 14:07:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, flash
+from flask import Flask,render_template, request, flash, sessions, redirect, url_for
 import sqlite3
 import uuid
 import bcrypt
